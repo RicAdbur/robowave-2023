e9fe8b3ad529e52d426780be18861d55e1293442
Max <maxrrice@hotmail.com>
2023-06-27 21:21:19 -0500

    glitch effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log('Hello robo!')
+// fullpage.js
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: "gplv3-license",
+        navigation: true,
+        navigationTooltips: ["Welcome", "New Robots", "Used Robots", "Contact"]
+    });
+
+    // typewriter.js
+var typewriterElements = document.querySelectorAll(".typewriter-effect")
+for (var i = 0; i < typewriterElements.length; i++) {
+    var currentEl = typewriterElements[i]
+    var innerText = currentEl.innerText
+    
+    new Typewriter(currentEl, {
+        loop: true,
+    }).typeString(innerText).pauseFor(1000).start()
+}
+
+// power glitch
+PowerGlitch.glitch('.glitch', {
+    hideOverflow: true
+})
+
+});
